8f801a0d6bbd4d83193cd52b713c00cdb0d90412
CPUPetet <c.nwodo@abujaprep.com>
2025-04-27 18:56:40 +0100

    operators

---
+++ operators.py
@@ -0,0 +1,13 @@
+num1 = 45
+num2 = 3
+
+print("Number1", num1)
+print("Number2", num2)
+print("Addition :", num1+num2)
+print("Difference :", num1-num2)
+print("Product :", num1*num2)
+print("Division :", num1/num2)
+print("Floor Division :", num1//num2)
+print("Modulus Operation :", num1%num2)
+print("Square :", num1**num2)
+print("Square Root :", num1**0.5)
