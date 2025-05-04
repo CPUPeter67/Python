6655e34cc0b0a6732bc9219022fb5b88c0fc444f
CPUPetet <c.nwodo@abujaprep.com>
2025-05-04 18:18:29 +0100

    condition

---
+++ conditionz.py
@@ -0,0 +1,11 @@
+number = int(input("Enter a number:"))#5
+print("The number is:", number)#
+
+if number > 0:
+    print("The number is positive")
+
+    if number < 0:
+        print("The number is negative")
+
+    else:
+        print("The number is zero") 
